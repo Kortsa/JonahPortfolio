ebb6db8c426ccbe8f6690365a9d967ef2ba4c57e
Kabanda Kpanti Michael <kabandakpantimichael@gmail.com>
2024-07-29 16:51:40 -0700

    worked on the responsiveness of navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,70 @@
+// import { Link, Outlet } from "react-router-dom";
+// import { useState } from "react";
+// import hamburger from "../../assets/icons8-hamburger-menu-50.png";
+// import hamburger2 from "../../assets/icons8-hamburger-button-50.png";
+// import "./Navbar.css";
+// function Navbar() {
+//   const [showMenu, setShowMenu] = useState(false);
+
+//   const toggleMenu = () => {
+//     setShowMenu(!showMenu);
+//   };
+//   const Lists = [
+//     {
+//       title: "Home",
+//       to: "/",
+//     },
+//     {
+//       title: "About",
+//       to: "/about",
+//     },
+
+//     {
+//       title: "Resume",
+//       to: "/resume",
+//     },
+//     {
+//       title: "Portfolio",
+//       to: "/portfolio",
+//     },
+//     {
+//       title: "Contact",
+//       to: "portfolio",
+//     },
+//   ];
+//   return (
+//     <>
+//       {/* <div className="navbar">
+//         <div className="list">
+//           {Lists.map((list, index) => {
+//             return (
+//               <Link key={index} to={list.to} className="link">
+//                 {list.title}
+//               </Link>
+//             );
+//           })}
+//           <img src={hamburger2} alt="" />
+//         </div>
+//         <Outlet />
+//       </div> */}
+
+//     </>
+//   );
+// }
+// export default Navbar;
+
 import { Link, Outlet } from "react-router-dom";
-import "./Navbar.css"
+import { useState } from "react";
+import hamburger2 from "../../assets/icons8-hamburger-button-50.png";
+import "./Navbar.css";
+
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   const Lists = [
     {
       title: "Home",
@@ -10,7 +74,6 @@ function Navbar() {
       title: "About",
       to: "/about",
     },
-
     {
       title: "Resume",
       to: "/resume",
@@ -24,21 +87,24 @@ function Navbar() {
       to: "portfolio",
     },
   ];
+
   return (
     <>
-      <div className="navbar">
+      <div className={`navbar ${showMenu ? "show" : ""}`} onClick={toggleMenu}>
+        <div className="hamburger-menu">
+          <img src={hamburger2} alt="Hamburger Menu" />
+        </div>
         <div className="list">
-          {Lists.map((list, index) => {
-            return (
-              <Link key={index} to={list.to} className="link">
-                {list.title}
-              </Link>
-            );
-          })}
+          {Lists.map((list, index) => (
+            <Link key={index} to={list.to} className="link">
+              {list.title}
+            </Link>
+          ))}
         </div>
         <Outlet />
       </div>
     </>
   );
 }
+
 export default Navbar;
