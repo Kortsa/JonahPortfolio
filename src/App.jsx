c8a55fa8631ae308ad0166ada28025b3a5ba9e39
Kabanda Kpanti Michael <kabandakpantimichael@gmail.com>
2024-08-01 15:58:27 -0700

    starting working on the resume page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 // import Navbar from "./Components/Navbar/Navbar";
--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,10 +1,24 @@
 import Navbar from "../Navbar/Navbar";
+import "./Resume.css";
 
 function Resume() {
   return (
     <>
       <Navbar />
-      <div>resume</div>
+      <div className="resume-contents">
+        <div className="container">
+          <h3>
+            Resume
+            <span>
+              <hr />
+            </span>
+          </h3>
+          <h1>Check my resume</h1>
+          <div className="summary">
+            <h3>summary</h3>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
